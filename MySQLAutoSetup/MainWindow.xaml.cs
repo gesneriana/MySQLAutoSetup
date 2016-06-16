f737e6e018a9f4f0a6ac27a960c42c7b9be9ae76
gesneriana <694060865@qq.com>
2016-06-16 19:17:08 +0800

    C#做的安装Mysql解压缩版的小工具

--- MySQLAutoSetup/MainWindow.xaml.cs
+++ MySQLAutoSetup/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,84 @@ namespace MySQLAutoSetup
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 生成my.ini配置文件的按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_auto_my_ini_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Directory.Exists(textBox_baseDir.Text.ToString()))
+            {
+                MessageBox.Show("不存在的mysql数据库根目录");
+                return;
+            }
+            string baseDir = textBox_baseDir.Text.ToString();
+
+            FileStream fs = new FileStream("my.ini", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("[client]\r\n");
+                sb.Append("port=3305\r\n");
+                sb.Append("default-character-set=utf8\r\n");
+                sb.Append("#客户端字符类型，与服务端一致就行，建议utf8\r\n");
+                sb.Append("[mysqld]\r\n");
+                sb.Append("port=3305\r\n");
+                sb.Append("character_set_server=utf8\r\n");
+                sb.Append("#服务端字符类型，建议utf8\r\n");
+                sb.Append("basedir=" + baseDir + "\r\n");
+                sb.Append("#解压根目录\r\n");
+                sb.Append("datadir=" + baseDir + "\\data\r\n");
+                sb.Append("#解压根目录\\data\r\n");
+                sb.Append("sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES\r\n");
+                sb.Append("[WinMySQLAdmin]\r\n");
+                sb.Append(baseDir + "\\bin\\mysqld.exe\r\n");
+                sb.Append("#解压根目录\\bin\\mysqld.exe\r\n");
+                sb.Append("#以上是复制内容，这行可不复制");
+                sw.Write(sb.ToString());
+                MessageBox.Show("my.ini配置文件生成成功");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sw != null) { sw.Close(); }
+                if (fs != null) { sw.Close(); }
+            }
+
+        }
+
+        /// <summary>
+        /// 生成自动安装MySql服务的脚本
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_auto_setup_server_Click(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists("my.ini")) { MessageBox.Show("my.ini配置文件不存在"); return; }
+            if (!Directory.Exists("bin")) { MessageBox.Show("找不到bin目录"); return; }
+            FileStream fs = new FileStream("bin\\auto_setup_server.cmd", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            try
+            {
+                sw.Write("mysqld --install");
+                MessageBox.Show("安装服务的命令脚本生成成功,\n打开当前路径下的bin目录,\n右键点击auto_setup_server.cmd文件,\n以管理员身份运行");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sw != null) sw.Close();
+                if (fs != null) fs.Close();
+            }
         }
     }
 }
